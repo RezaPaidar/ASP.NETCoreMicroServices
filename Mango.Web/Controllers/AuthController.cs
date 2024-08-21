6d2fee4fa949d337f549a9abf8b50e2dfe3d42ff
RezaPaidar <reza.paidar@ooutlook.com>
2024-08-21 20:06:16 +0330

    chore-- add login and register validation

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,19 +1,26 @@
-﻿using Mango.Web.Models;
+﻿using Mango.Services.AuthAPI.Models;
+using Mango.Web.Models;
 using Mango.Web.Services.IService;
 using Mango.Web.Utility;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Mango.Web.Controllers
 {
     public class AuthController : Controller
     {
         private readonly IAuthService _authService;
+        private readonly ITokenProvider _tokenProvider;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService, ITokenProvider tokenProvider)
         {
             _authService = authService;
+            _tokenProvider = tokenProvider; 
         }
 
         [HttpGet]
@@ -33,11 +40,13 @@ namespace Mango.Web.Controllers
                 LoginResponseDTO loginResponseDTO = 
                     JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(responseDto.Result));
 
+                await SignInUser(loginResponseDTO);
+                _tokenProvider.SetToken(loginResponseDTO.Token);
                 return RedirectToAction("Index", "Home");
             }
             else
             {
-                ModelState.AddModelError("customerError", responseDto.Message);
+                TempData["error"] = responseDto.Message;
                 return View(obj);
             }
         }
@@ -72,16 +81,10 @@ namespace Mango.Web.Controllers
                     TempData["success"] = "Registration is successfull.";
                     return RedirectToAction(nameof(Login));
                 }
-                else
-                {
-                    ModelState.AddModelError("customerError", assignRole.Message);
-                    return View(obj);
-                }
             }
             else
             {
-                ModelState.AddModelError("customerError", result.Message);
-                return View(obj);
+                TempData["error"] = result.Message;
             }
 
             var roleList = new List<SelectListItem>()
@@ -97,6 +100,28 @@ namespace Mango.Web.Controllers
         public IActionResult Logout()
         {
             return View();
+        }
+
+        private async Task SignInUser(LoginResponseDTO model)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(model.Token);
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Email,
+                jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Email).Value));
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub,
+                jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub).Value));
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Name,
+                jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Name).Value));
+
+            identity.AddClaim(new Claim(ClaimTypes.Name,
+                jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Email).Value));
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal); 
+
         }
     }
 }

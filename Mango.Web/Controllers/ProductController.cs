69a4162a0cbc7533fcc4722564ca570a6fc6f4ae
RezaPaidar <reza.paidar@ooutlook.com>
2024-08-27 09:21:45 +0330

    chore-- product UI

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -51,7 +51,34 @@ namespace Mango.Web.Controllers
             }
             return View(productDto);
         }
-        public async Task<IActionResult> ProductDelete(int productId)
+		public async Task<IActionResult> ProductEdit(int productId)
+		{
+			ResponseDTO? response = await _productService.GetProductByIdAsync(productId);
+			if (response != null && response.IsSuccess)
+			{
+				TempData["success"] = "Product updated successfully!";
+				ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result) ?? string.Empty);
+				return View(model);
+			}
+			return NotFound();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ProductEdit(ProductDto productDto)
+		{
+			ResponseDTO? response = await _productService.UpdateProductsAsync(productDto);
+			if (response != null && response.IsSuccess)
+			{
+				return RedirectToAction(nameof(ProductIndex));
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
+			return View(productDto);
+		}
+
+		public async Task<IActionResult> ProductDelete(int productId)
         {
             ResponseDTO? response = await _productService.GetProductByIdAsync(productId);
             if (response != null && response.IsSuccess)

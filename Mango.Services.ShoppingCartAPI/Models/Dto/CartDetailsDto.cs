ec0c7d12f629050a0b57150333de777d35b3c827
RezaPaidar <reza.paidar@ooutlook.com>
2024-08-27 13:05:03 +0330

    chore-- CartApiService

---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDetailsDto
+    {
+        public int CardDetailsId { get; set; }
+        public int CardHeaaderId { get; set; }
+        public CartHeader? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
